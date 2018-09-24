12eefd259faa01ed4126904f0429dce5dc4d361a
Ilia Barashkov <ibarashkov@list.ru>
2018-09-25 00:13:08 +0300

    task 1.2

---
+++ index.js
@@ -0,0 +1,61 @@
+class Project {
+  constructor(name, author, version = `v0.0.1`) {
+    this.name = name;
+    this.author = author;
+    this.version = version;
+    this.input = process.argv[2];
+    this.errorColor = `\x1b[31m`;
+
+    this.validCommands = [`help`, `version`];
+    this.commands = {
+      help: {
+        text: `
+    Доступные комманды:
+    --help    - печатает этот текст;
+    --version - печатает версию приложения`,
+        code: 0
+      },
+      version: {
+        text: this.version,
+        code: 0
+      },
+      '': {
+        text: `
+    Привет пользователь!
+    Эта программа будет запускать сервер "${this.name}".
+    Автор: ${this.author}`,
+        code: 0
+      },
+      wrong: {
+        text: `
+    Неизвестная комманда ${this.input}.
+    Чтобы прочитать правила использования приложения, 
+    наберите "--help"`,
+        code: 1,
+        color: this.errorColor
+      }
+    };
+    this.init();
+  }
+
+  init() {
+    this.input = this.input ? this.input.slice(2) : ``;
+    if (this.validCommands.includes(this.input) || this.input === ``) {
+      this.printMessage(this.commands[this.input].text, this.commands[this.input].code);
+    } else {
+      this.printError(this.commands.wrong.color, this.commands.wrong.text, this.commands.wrong.code);
+    }
+  }
+
+  printMessage(message, code) {
+    console.log(message);
+    process.exit(code);
+  }
+
+  printError(color, message, code) {
+    console.error(color, message);
+    process.exit(code);
+  }
+}
+
+const project = new Project('Кекстаграм', 'Кекс');
