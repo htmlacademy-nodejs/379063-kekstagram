c5911da8501965f43666c3a0f184ac07b0883bc0
Ilia Barashkov <ibarashkov@list.ru>
2018-10-04 21:27:07 +0300

    task 2.3 refactor

--- index.js
+++ index.js
@@ -8,19 +8,16 @@ const license = require(`./src/license`);
 const commonInfo = require(`./src/common-info`);
 const wrongCommand = require(`./src/wrong-command`);
 
-const commands = {};
-const input = process.argv[2] ? process.argv[2] : `empty`;
+const commands = {help, author, version, description, license, wrongCommand, commonInfo};
 
-Object.defineProperty(commands, `--help`, {value: help.execute.bind(help)});
-Object.defineProperty(commands, `--version`, {value: version.execute.bind(version)});
-Object.defineProperty(commands, `--author`, {value: author.execute.bind(author)});
-Object.defineProperty(commands, `--license`, {value: license.execute.bind(license)});
-Object.defineProperty(commands, `--description`, {value: description.execute.bind(description)});
-Object.defineProperty(commands, `wrong`, {value: wrongCommand.execute.bind(wrongCommand)});
-Object.defineProperty(commands, `empty`, {value: commonInfo.execute.bind(commonInfo)});
+const checkCommand = (command) => {
+  return (command.slice(0, 2) === `--` && command.slice(2) in commands) ? command.slice(2) : command;
+};
+
+const input = process.argv[2] ? checkCommand(process.argv[2]) : `commonInfo`;
 
 if (input in commands) {
-  commands[input]();
+  commands[input].execute();
 } else {
-  commands[`wrong`](input);
+  commands[`wrongCommand`].execute(input);
 }
